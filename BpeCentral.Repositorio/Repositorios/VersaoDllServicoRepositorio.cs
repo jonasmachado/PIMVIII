f832bf8ee12e0c90bcd401310c56ad1ab4ff034c
Jonas Machado <jonasm.1511@gmail.com>
2019-11-17 04:04:34 -0300

    deleting useless stuff

--- BpeCentral.Repositorio/Repositorios/VersaoDllServicoRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using BpeCentral.Dominio;
-using BpeCentral.Dominio.Repositorio.Interfaces;
-
-namespace BpeCentral.Repositorio.Repositorios
-{
-    public class VersaoDllServicoRepositorio : BaseCrudDao<BPE_VERSAO_DLL_SERVICO>, IVersaoDllServicoRepositorio
-    {
-    }
-}
